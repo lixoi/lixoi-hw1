090e081a390ff1002fde1f366886b7aff32c740f
Aleksey Lixoi <al_dashing@list.ru>
2023-02-06 00:30:40 +0300

    HW2 is completed

--- hw02_unpack_string/go.mod
+++ hw02_unpack_string/go.mod
@@ -1,4 +1,4 @@
-module github.com/fixme_my_friend/hw02_unpack_string
+module github.com/lixoi/lixoi-hw1
 
 go 1.19
 
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,49 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
+const (
+	// ServiceSymbol ...
+	ServiceSymbol = '\\'
+)
+
+// ErrInvalidString ...
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func isServiceSymbol(ra []rune, index *int) bool {
+	if ra[*index] == rune(ServiceSymbol) &&
+		(unicode.IsDigit(ra[*index+1]) || ra[*index+1] == rune(ServiceSymbol)) {
+		*index++
+		return true
+	}
+
+	return false
+}
+
+// Unpack ...
+func Unpack(str string) (string, error) {
+	var resultStr strings.Builder
+	ra := []rune(str)
+	for i := 0; i < len(ra); i++ {
+		if !isServiceSymbol(ra, &i) && unicode.IsDigit(ra[i]) {
+			return "", ErrInvalidString
+		}
+		if i < len(ra)-1 {
+			if unicode.IsDigit(ra[i+1]) {
+				number, _ := strconv.Atoi(string(ra[i+1]))
+				for j := 0; j < number; j++ {
+					resultStr.WriteRune(ra[i])
+				}
+				i++
+				continue
+			}
+		}
+		resultStr.WriteRune(ra[i])
+	}
+
+	return resultStr.String(), nil
 }
