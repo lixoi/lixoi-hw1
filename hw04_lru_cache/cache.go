4d9de64f565fdfadd1c7bfb802faa8613735318e
Aleksey Lixoi <al_dashing@list.ru>
2023-02-27 22:48:59 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -19,6 +19,11 @@ type lruCache struct {
 	items    map[Key]*ListItem
 }
 
+type valueItem struct {
+	val interface{}
+	k   Key
+}
+
 // NewCache ...
 func NewCache(capacity int) Cache {
 	return &lruCache{
@@ -35,19 +40,20 @@ func (lc *lruCache) Set(key Key, value interface{}) bool {
 	lc.mu.Lock()
 	defer lc.mu.Unlock()
 	if v, ok := lc.items[key]; ok {
-		v.Value = value
+		v.Value = valueItem{
+			val: value,
+			k:   key,
+		}
 		lc.queue.MoveToFront(v)
 		return true
 	}
-	lc.items[key] = lc.queue.PushFront(value)
+	lc.items[key] = lc.queue.PushFront(valueItem{
+		val: value,
+		k:   key,
+	})
 	if lc.queue.Len() > lc.capacity {
 		pntr := lc.queue.Back()
-		for k, v := range lc.items {
-			if v == pntr {
-				delete(lc.items, k)
-				break
-			}
-		}
+		delete(lc.items, pntr.Value.(valueItem).k)
 		lc.queue.Remove(pntr)
 	}
 
@@ -62,7 +68,7 @@ func (lc *lruCache) Get(key Key) (interface{}, bool) {
 	defer lc.mu.Unlock()
 	if v, ok := lc.items[key]; ok {
 		lc.queue.MoveToFront(v)
-		return v.Value, ok
+		return v.Value.(valueItem).val, ok
 	}
 	return nil, false
 }
